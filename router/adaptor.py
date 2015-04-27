8ba8dd46f76037539f6f1992c6ffbc328d9796d0
Zhengqi Yang <robbenyang@gmail.com>
2015-04-27 05:18:53 -0500

    using simpler rules for adaptor

--- router/adaptor.py
+++ router/adaptor.py
@@ -41,7 +41,28 @@ def adaptor(remote_info, insNetworking, dispatcher, hardware_info):
 	ret = {}
 	ret["type"] = "thor"
 	ret["throttling"] = remote_throttling
+	num_jobs_in = 0
+	if local_info["status"] == "Done" and remote_info["status"] == "Done":
+		ret["type"] = "Done"
+	elif local_info["status"] == "Done": 
+		num_jobs_in = remote_info["num"] * local_info["throttling"]/(local_info["throttling"] + remote_info["throttling"])
+	elif remote_info["status"] == "Done": 
+		num_jobs_in = (-1) * remote_info["num"] * local_info["throttling"]/(local_info["throttling"] + remote_info["throttling"])
+	else: 
+		if local_info["num"] - remote_info["num"] > 20: 
+			num_jobs_in = (-1) * (local_info - remote_info) / 2 
+		elif remote_info["num"] - local_info["num"] > 20: 
+			num_jobs_in = (remote_info - local_info) / 2 
 
+	if num_jobs_in > 0: 
+		ret["reqJobs"] = num_jobs_in
+	else: 
+		ret["reqJobs"] = 1
+		# Call transfer manager
+
+	insNetworking.send_comm(ret)
+
+'''
 	local_speed = local_info["num"] / local_info["time"] * (new_throttling / local_info["throttling"])
 <<<<<<< HEAD:adaptor.py
 	if local_speed == 0:
@@ -58,27 +79,4 @@ def adaptor(remote_info, insNetworking, dispatcher, hardware_info):
 	local_rem = local_info["num"] / local_speed 
 >>>>>>> 86290a4b61034b83b5a61945b0afa9dc9e82315d:router/adaptor.py
 	remote_rem = remote_info["num"] / remote_speed 
-
-	num_jobs_in = 0
-	if local_info["status"] == "Done" and remote_info["status"] == "Done":
-		ret["type"] = "Done"
-	elif local_info["status"] == "Done": 
-		num_jobs_in = remote_info["num"] * local_info["throttling"]/(local_info["throttling"] + remote_info["throttling"])
-	
-	elif remote_info["status"] == "Done": 
-		num_jobs_in = (-1) * remote_info["num"] * local_info["throttling"]/(local_info["throttling"] + remote_info["throttling"])
-
-	else: 
-		if local_rem - remote_rem > 20: 
-			num_jobs_in = (-1) * (local_rem - remote_rem) * remote_speed / 2 
-		elif remote_rem - local_rem > 20: 
-			num_jobs_in = (remote_rem - local_rem) * local_speed / 2 
-
-	if num_jobs_in > 0: 
-		ret["reqJobs"] = num_jobs_in
-	else: 
-		ret["reqJobs"] = 1
-		# Call transfer manager
-
-	insNetworking.send_comm(ret)
-
+'''
